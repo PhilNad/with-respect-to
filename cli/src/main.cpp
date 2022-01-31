d268658ae16478cb9d65f20d2d0c8eac373c4135
PhilNad <philippe.nadeau1991@gmail.com>
2022-01-30 21:14:14 -0500

    Added some tests, does crash, to investigate and document.

--- cli/src/main.cpp
+++ cli/src/main.cpp
@@ -3,12 +3,19 @@
 #include <SQLiteCpp/SQLiteCpp.h>
 #include <string>
 #include <cfloat>
+#include <math.h>
 #include "Wrt.h"
 
 using namespace std;
 using Eigen::Affine3d;
 using Eigen::Matrix3d;
+using Eigen::AngleAxisd;
+using Eigen::Vector3d;
  
+double deg_to_rad(double angle){
+    return angle * M_PI / 180;
+}
+
 int main()
 {
     Affine3d m = Affine3d::Identity();
@@ -24,21 +31,34 @@ int main()
     Affine3d pose;
     pose.matrix() << 1,0,0,1, 0,1,0,1, 0,0,1,1, 0,0,0,1;
     wrt.In("test").Set("a").Wrt("world").Ei("world").As(pose);
-    wrt.In("test").Get("a").Wrt("world").Ei("world");
-    /* TODO, further testing.
+    pose = wrt.In("test").Get("a").Wrt("world").Ei("world");
+
+    Matrix3d rot;
+    rot = AngleAxisd(deg_to_rad(90), Vector3d::UnitX());
+    pose.linear() = rot;
+    pose.translation() << 0,0,0;
+    cout << pose.matrix() << endl;
+    wrt.In("test").Set("b").Wrt("a").Ei("a").As(pose);
     
-    db = WRT.DbConnector()
-    pose = np.array([[1,0,0,1],[0,1,0,1],[0,0,1,1],[0,0,0,1]])
-    db.In('test').Set('a').Wrt('world').Ei('world').As(pose)
+    rot = Matrix3d::Identity();
+    pose.linear() = rot;
+    pose.translation() << 1,0,0;
+    cout << pose.matrix() << endl;
+    wrt.In("test").Set("c").Wrt("b").Ei("b").As(pose);
 
-    pose = SE3.Rx(90, "deg")
-    db.In('test').Set('b').Wrt('a').Ei('a').As(pose)
+    rot = AngleAxisd(deg_to_rad(90), Vector3d::UnitZ());
+    pose.linear() = rot;
+    pose.translation() << 1,1,0;
+    cout << pose.matrix() << endl;
+    wrt.In("test").Set("d").Wrt("b").Ei("b").As(pose);
 
-    pose = SE3.Rx(0, "deg", t=[1,0,0])
-    db.In('test').Set('c').Wrt('b').Ei('b').As(pose)
+    pose.matrix() << 1,0,0,0, 0,0,1,0, 0,-1,0,0, 0,0,0,1;
+    assert(wrt.In("test").Get("a").Wrt("b").Ei("b").matrix().isApprox(pose.matrix()));
+    /*assert(wrt.In("test").Get("a").Wrt("b").Ei("a")          == SE3(np.eye(4)))
+    assert(wrt.In("test").Get("c").Wrt("world").Ei("world")  == SE3(np.array([[1,0,0,2],[0,0,-1,1],[0,1,0,1],[0,0,0,1]])))
+    assert(wrt.In("test").Get("c").Wrt("world").Ei("c")      == SE3(np.array([[1,0,0,2],[0,1,0,1],[0,0,1,-1],[0,0,0,1]])))
+    assert(wrt.In("test").Get("c").Wrt("world").Ei("a")      == SE3(np.array([[1,0,0,2],[0,0,-1,1],[0,1,0,1],[0,0,0,1]])))
+    assert(wrt.In("test").Get("d").Wrt("a").Ei("a")          == SE3(np.array([[0,-1,0,1],[0,0,-1,0],[1,0,0,1],[0,0,0,1]])))*/
 
-    pose = SE3.Rz(90, "deg", t=[1,1,0])
-    db.In('test').Set('d').Wrt('b').Ei('b').As(pose)
-    */
 
 }
